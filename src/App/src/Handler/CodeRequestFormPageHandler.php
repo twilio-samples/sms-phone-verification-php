2bb25b5bef0f96069e2bfa7e8f6e274b319250c5
Matthew Setter <matthew@matthewsetter.com>
2024-09-13 21:57:34 +1000

    Clean up the default page handler and its template

--- src/App/src/Handler/CodeRequestFormPageHandler.php
+++ src/App/src/Handler/CodeRequestFormPageHandler.php
@@ -4,30 +4,16 @@ declare(strict_types=1);
 
 namespace App\Handler;
 
-use Chubbyphp\Container\MinimalContainer;
-use DI\Container as PHPDIContainer;
 use Laminas\Diactoros\Response\HtmlResponse;
-use Laminas\Diactoros\Response\JsonResponse;
-use Laminas\ServiceManager\ServiceManager;
-use Mezzio\LaminasView\LaminasViewRenderer;
-use Mezzio\Plates\PlatesRenderer;
-use Mezzio\Router\FastRouteRouter;
-use Mezzio\Router\LaminasRouter;
-use Mezzio\Router\RouterInterface;
 use Mezzio\Template\TemplateRendererInterface;
-use Mezzio\Twig\TwigRenderer;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class CodeRequestFormPageHandler implements RequestHandlerInterface
+readonly final class CodeRequestFormPageHandler implements RequestHandlerInterface
 {
-    public function __construct(
-        private string $containerName,
-        private RouterInterface $router,
-        private ?TemplateRendererInterface $template = null
-    ) {
+    public function __construct(private TemplateRendererInterface $template)
+    {
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
