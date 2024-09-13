9d24f931c6bdc381eeca4103a999c0bbec3c00fd
Matthew Setter <matthew@matthewsetter.com>
2024-09-13 16:44:17 +1000

    Autoload the Twilio credentials into the application's configuration
    These are required for making requests to Twilio, so it makes sense to
    load them as trivially as possible.

---
+++ config/autoload/twilio.global.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+// phpcs:disable PSR12.Files.FileHeader.IncorrectOrder
+
+/**
+ * Through the magic of PHPDotenv, this file will make your Twilio credentials available in the
+ * application's configuration.
+ */
+
+return [
+    'twilio' => [
+        'account_sid' => $_ENV['TWILIO_ACCOUNT_SID'],
+        'auth_token'  => $_ENV['TWILIO_AUTH_TOKEN'],
+    ],
+];
